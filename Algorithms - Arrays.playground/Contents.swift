a066a56842fe512b821fbfa8b07cdbdc04c44540
Dominique Bezerra <dominique.bezerra@ifood.com.br>
2025-03-13 09:53:10 -0300

    Two sum solution with two for loops

--- Algorithms - Arrays.playground/Contents.swift
+++ Algorithms - Arrays.playground/Contents.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-/* Two Sum: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- 
+/*
+ Two Sum:
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
  You can return the answer in any order.
 
  Example:
@@ -14,29 +14,27 @@ import Foundation
 
  Constraints:
      2 <= nums.length <= 10^4
-      -10^9 <= nums[i] <= 10^9
-      -10^9 <= target <= 10^9
- Only one valid answer exists. */
+     -10^9 <= nums[i] <= 10^9
+     -10^9 <= target <= 10^9
+     Only one valid answer exists.
+ */
 
 class Solution {
    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-       var firstIndex = 0
-       var savedIndex = 0
-       for (index, element) in nums.enumerated() {
-           if nums[savedIndex] + element == target && savedIndex != index {
-               return [savedIndex, index]
-           } else if nums[firstIndex] + element == target && firstIndex != index {
-               return [firstIndex, index]
-           } else {
-               savedIndex = index
+       for (firstIndex, _) in nums.enumerated() {
+           for (index, element) in nums.enumerated() {
+               if nums[firstIndex] + nums[index] == target && firstIndex != index {
+                   return [firstIndex, index]
+               }
            }
        }
        return []
    }
 }
 
-let case1 = Solution().twoSum([2,7,11,15], 9)
-let case2 = Solution().twoSum([3,2,4], 6)
-let case3 = Solution().twoSum([3,3], 6)
-let problem = Solution().twoSum([3,2,3], 6)
+let case1 = Solution().twoSum([2,7,11,15], 9) // [0, 1]
+let case2 = Solution().twoSum([3,2,4], 6) // [1, 2]
+let case3 = Solution().twoSum([3,3], 6) // [0, 1]
+let failingTestCase = Solution().twoSum([3,2,3], 6) // [0, 2]
+let problem = Solution().twoSum([-1,-2,-3,-4,-5], -8) // [2, 4]
 print(problem)
